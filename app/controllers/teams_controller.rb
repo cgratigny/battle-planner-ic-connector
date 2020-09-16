9669d15fbfb565352b499f0dd07bd57cad0e8369
Chris Gratigny <chrisg@ibethel.org>
2020-09-15 21:27:18 -0700

    Add teams.

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,4 +1,4 @@
-class MembersController < ApplicationController
+class TeamsController < ApplicationController
   before_action :set_member, only: [:show, :update, :destroy]
   before_action :build_collection, only: :index
   before_action :authenticate!
@@ -7,20 +7,20 @@ class MembersController < ApplicationController
   require 'net/http'
   require 'active_support/core_ext/hash'
 
-  # GET /members
+  # GET /teams
   def index
-    render json: @members
+    render json: @teams
   end
 
   private
 
     def build_collection
       xml = Net::HTTP.get_response(URI.parse(podio_url)).body
-      @members = Hash.from_xml(xml).to_json
+      @teams = Hash.from_xml(xml).to_json
     end
 
     def podio_url
-      Rails.application.credentials.dig(:podio_members_url)
+      Rails.application.credentials.dig(:podio_teams_url)
     end
 
 end
