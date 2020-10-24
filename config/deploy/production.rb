fabf324f4c90282f38004662f89497c792fea178
Chris Gratigny <chrisg@ibethel.org>
2020-10-24 05:03:00 -0700

    Update deploy.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,7 +5,7 @@ set :stage, :production
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server '161.35.127.15', user: 'deploy', roles: %w{web}
+server 'battleplantracker.com', user: 'deploy', roles: %w{web}
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
