53fb8792cc4b135caf6bdded49fa88b0ff56fb69
Chris Gratigny <cgratigny>
2023-12-29 00:59:41 -0800

    Add dark mode + update firestore service.

--- app/services/firestore_service.rb
+++ app/services/firestore_service.rb
@@ -7,33 +7,39 @@ class FirestoreService < ApplicationService
   end
 
   def sync_users
+    puts " = Sync Users".black.bold
     users_collection.get.each do |firestore_user|
-      # begin
+      puts " - Process #{firestore_user.get(:email)}".gray
+      begin
         Firestore::User.sync_from_firestore_user(firestore_user)
-      # rescue => e
-      #   ap e.message
-      # end
+      rescue => e
+        Honeybadger.notify(e)
+      end
     end
   end
 
   def sync_plans
+    puts " = Sync Plans".black.bold
     users_collection.get.each do |firestore_user|
-      # begin
+      puts " - Process #{firestore_user.get(:email)}".gray
+      begin
         Firestore::BattlePlan.sync_from_firestore_user(firestore_user, date)
-      # rescue => e
-      #   ap e.message
-      # end
+      rescue => e
+        Honeybadger.notify(e)
+      end
     end
   end
 
   def sync_progress
+    puts " = Sync Progress".black.bold
     users_collection.get.each do |firestore_user|
-      # begin
+      puts " - Process #{firestore_user.get(:email)}".gray
+      begin
         plan = Firestore::BattlePlan.by_firestore_user(firestore_user).by_date(date).first
         plan.sync_for_date(date) if plan.present?
-      # rescue => e
-      #   ap e.message
-      # end
+      rescue => e
+        Honeybadger.notify(e)
+      end
     end
   end
 
@@ -48,5 +54,6 @@ class FirestoreService < ApplicationService
     collection.where(:team_id, :"=", team.team_id) if team.present?
     collection
   end
+
 
 end
