5e27b2ffdb4811c97c8b4d97ed1264ded890f4e0
Chris Gratigny <cgratigny>
2023-12-30 14:23:37 -0800

    Add privacy page.

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  get "privacy" => "welcome#privacy"
   get "example-vision" => "welcome#example_vision"
   get "iron-council" => "welcome#iron_council"
   root "welcome#show"
