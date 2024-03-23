80c2ad602bd53622a06369d264f3404b63b0b348
cgratigny <chris@gratigny.com>
2024-03-23 07:07:45 -0700

    update rails

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,8 @@ module BattlePlanConnector
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
 
+    config.active_support.cache_format_version = 7.1
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
